31e2208209eabbb2157227cc39ef02c06146b3b8
HuseyinDOGAN <huseyindgn00@gmail.com>
2022-06-11 11:07:47 +0300

    login register butonlarının düzenlenmesi

--- lib/view/entry_view.dart
+++ lib/view/entry_view.dart
@@ -104,53 +104,23 @@ class _EntryViewState extends State<EntryView> {
   }
 
   btnLogin() {
-    return ElevatedButton(onPressed: () {}, child: Text('Giriş'));
+    return ElevatedButton(
+      child: Text('Giriş'),
+      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey.shade800)),
+      onPressed: () async {
+        // KAYITLI BİR USER VARSA GİRİŞ YAP
+        bool result = await entryViewModel.login(_controllerUserName.text, _controllerPassword.text).then((value) {
+          return value;
+        });
+        if (result) {
+          Utility.getEasyLoading(miliSeconds: 1500, showSate: EasyLoadingShowState.showDefault);
+          Navigator.pushReplacementNamed(context, '/home');
+        } else {
+          Utility.getEasyLoading(showSate: EasyLoadingShowState.showInfo, miliSeconds: 1000);
+        }
+      },
+    );
   }
 }
 
 // BUTONUN USER OLUP OLMAMASINA GÖRE YAZILDIĞI YER
-class ButtonLogin extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 100,
-        height: 40,
-        margin: EdgeInsets.all(25),
-        child: _entryAndSaveButton(context),
-      ),
-    );
-  }
-
-  ElevatedButton _entryAndSaveButton(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey.shade800)),
-      onPressed: () async {
-        // KAYITLI BİR USER VARSA GİRİŞ YAP
-        if (_isCurrent) {
-          bool result = await entryViewModel.login(_controllerUserName.text, _controllerPassword.text).then((value) {
-            return value;
-          });
-          if (result) {
-            Utility.getEasyLoading(miliSeconds: 1500, showSate: EasyLoadingShowState.showDefault);
-            Navigator.pushReplacementNamed(context, '/home');
-          } else {
-            Utility.getEasyLoading(showSate: EasyLoadingShowState.showInfo, miliSeconds: 1000);
-          }
-        }
-        // KAYITLI BİR USER YOKSA KAYDET
-      },
-      child: _isCurrent
-          ? Text(
-              'Giriş',
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            )
-          : Text(
-              'Kayıt Ol',
-              style: TextStyle(fontSize: 15),
-            ),
-    );
-  }
-}
