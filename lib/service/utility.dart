c2b7ab6294cf4bb89d4243ce12a7400ec348572b
HuseyinDOGAN <huseyindgn00@gmail.com>
2022-06-11 12:14:24 +0300

    kayıt butonuna uyarı aksiyonu eklendi

--- lib/service/utility.dart
+++ lib/service/utility.dart
@@ -5,19 +5,15 @@ import '../model/easyloadin_show_state.dart';
 mixin Utility {
   String getDate(String? milliseconds) {
     int _milliseconds = int.parse(milliseconds!);
-    String day =
-        DateTime.fromMillisecondsSinceEpoch(_milliseconds).day.toString();
-    String month =
-        DateTime.fromMillisecondsSinceEpoch(_milliseconds).month.toString();
-    String year =
-        DateTime.fromMillisecondsSinceEpoch(_milliseconds).year.toString();
+    String day = DateTime.fromMillisecondsSinceEpoch(_milliseconds).day.toString();
+    String month = DateTime.fromMillisecondsSinceEpoch(_milliseconds).month.toString();
+    String year = DateTime.fromMillisecondsSinceEpoch(_milliseconds).year.toString();
 
     return '$day.$month.$year';
   }
 
   static void getEasyLoading(
-      {required EasyLoadingShowState showSate,
-      required int miliSeconds}) async {
+      {required EasyLoadingShowState showSate, required int miliSeconds, required String expression}) async {
     switch (showSate) {
       case EasyLoadingShowState.showDefault:
         EasyLoading.show(dismissOnTap: false);
